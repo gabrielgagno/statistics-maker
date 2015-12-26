75e17611016b82b4989e8b2afb80d6f2641356de
isko-algorithm <gjpgagno@gmail.com>
2015-12-26 19:48:21 +0800

    Initial commit

---
+++ StatisticsMaker.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dell
+ * Date: 12/26/2015
+ * Time: 2:32 PM
+ */
+
+namespace StatisticsMaker;
+
+class StatisticsMaker
+{
+    public static function metadataStatistics() {
+        $conn = mysqli_connect('localhost', 'root', '', 'nutch');
+        if(!$conn) {
+            die(mysqli_error($conn));
+        }
+
+        $query = "select baseUrl from webpage where aq_md_description is null or aq_md_description like '%%'".
+            "or aq_md_searchstring is null or aq_md_searchstring like '%%'";
+
+        $results = $conn->query($query);
+
+        foreach(mysqli_fetch_assoc($results) as $row) {
+            echo $row['baseUrl']."\n";
+        }
+    }
+}
---
+++ meta-finder.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dell
+ * Date: 12/26/2015
+ * Time: 2:31 PM
+ */
+
+ namespace StatisticsMaker;
+
+use StatisticsMaker\StatisticsMaker;
+
